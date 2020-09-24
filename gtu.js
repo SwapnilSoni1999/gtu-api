2cb79b118e56d6ab5b0526aae2b7d7c801e97a5a
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2020-09-24 13:37:20 +0530

    fromEnrollment: get result from enrollment and examId

--- gtu.js
+++ gtu.js
@@ -2,6 +2,8 @@ const { defaultHeaders, hostUrl } = require('./config')
 const { default: axios } = require('axios')
 const qs = require('querystring')
 
+const generateDeviceId = require('./utils/deviceId')
+
 class GTUResults {
     constructor() { }
 
@@ -47,6 +49,30 @@ class GTUResults {
             data: qs.stringify(data),
             responseType: 'json'
         })
+        return res.data
+    }
+
+    static async fromEnrollment(enrollment, examId) {
+        const data = {
+            'ReqOperation': 'StudentResult',
+            'ExamID': String(examId),
+            'EnrNo': String(enrollment),
+            'DeviceId': generateDeviceId(),
+            'OSversion': '29',
+            'LatLong': '0',
+            'MobileNo': '916929696969',
+            'IMEI_NO': '0',
+            'IPAddress': '7a54:8059:4d78:551f:6065:9eda:7720:f6eb%dummy0',
+            'IsCurrent': '0'
+        }
+        const res = await axios({
+            method: 'POST',
+            url: hostUrl,
+            headers: defaultHeaders,
+            data: qs.stringify(data),
+            responseType: 'json'
+        })
+
         return res.data
     }
 }
